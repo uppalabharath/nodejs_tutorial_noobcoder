6fea8cdbac597123d2e5589b7cecf6e613842590
Bharath Kumar Uppala <uppala.bharath@gmail.com>
2018-12-17 21:03:01 +0530

    aAdded link for express use documentation

--- express_tutorial/express_static_path.js
+++ express_tutorial/express_static_path.js
@@ -1,10 +1,12 @@
 // static file serving using express
+// http://expressjs.com/en/4x/api.html#app.use
 
 const express = require("express");
 const path = require("path");
 
 const app = express();
 
+// use is used add any middleware processing before passing / calling routes
 // creating an alias for static folder
 app.use("/public", express.static(path.join(__dirname, "static")));
 
